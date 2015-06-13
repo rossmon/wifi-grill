0c1a7365cce46d4dd048539cf43a4396bd0dc5fc
Ross Montague <ross@montagues.us>
2015-06-13 17:20:34 -0500

    updatesss

---
+++ db/migrate/20150613221205_create_model.rb
@@ -0,0 +1,14 @@
+class CreateModel < ActiveRecord::Migration
+ def up
+  create_table :models do |t|
+   t.string :name
+  end
+ end
+
+ def down
+  drop_table :models
+ end
+
+ # def change
+ # end
+end
