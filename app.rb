c3ca9b0a24c1aade2921cbd91dd19fec6a017839
rossmon <ross@montagues.us>
2015-06-13 20:01:54 +0000

    Update app.rb

--- app.rb
+++ app.rb
@@ -1,6 +1,12 @@
-require 'sinatra'
 
-get '/' do
-	puts 'hello world'
+post '/post' do
+	#create a post
 end
 
+put '/post/:id' do
+	#update a post
+end
+
+delete '/post/:id' do
+	#delete a post
+end
